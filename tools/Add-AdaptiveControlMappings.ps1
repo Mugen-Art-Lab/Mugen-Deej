26a5f4bfedceb459150b3aff56431e500dde37c7
MrSoichi <21004580+MrSoichi@users.noreply.github.com>
2026-09-27 02:22:28 +0600

    ui: accent first-run inversion hint

--- tools/Add-AdaptiveControlMappings.ps1
+++ tools/Add-AdaptiveControlMappings.ps1
@@ -3030,7 +3030,7 @@ function Show-FirstRunWizard {
     $wizard.Controls.Add($nextHintLine2)
 
     $nextHintLine3 = New-Object System.Windows.Forms.Label
-    $nextHintLine3.ForeColor = [System.Drawing.Color]::DimGray
+    $nextHintLine3.ForeColor = [System.Drawing.Color]::FromArgb(230, 170, 70)
     $nextHintLine3.Location = New-Object System.Drawing.Point(27, 310)
     $nextHintLine3.Size = New-Object System.Drawing.Size(606, 22)
     $nextHintLine3.Visible = $false
